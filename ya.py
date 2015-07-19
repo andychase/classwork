a76e29c585bc835d9ca36a55ef87c069bcbc5a22
Andy Chase <asperous2@gmail.com>
2015-07-18 20:22:01 -0700

    Update room generation

--- ya.py
+++ ya.py
@@ -1,6 +1,6 @@
-import sys
 import os
 import random
+import shutil
 
 # Config
 
@@ -17,9 +17,11 @@ room_connection_format = \
 
 room_types = ["MID_ROOM", "START_ROOM", "END_ROOM"]
 
-room_names = "XYZZY PLUGH PLOVER twisty Zork Crowther Dungeon".split()
+room_names = ['XYZZY', 'PLUGH', 'PLOVER', 'twisty', 'Zork', 'Crowther', 'Dungeon']
 
-room_names2 = room_names[:]
+rooms_left = room_names[:]
+
+room_connections = [[], [], [], [], [], [], []]
 
 number_of_rooms = 7
 
@@ -43,10 +45,39 @@ YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:
 
 steps = 0
 
+buffer = ""
 
-def build_a_room(f, the_dir_name, room_name, start=False, end=False):
-    number_of_connections = random.randint(connections_min, connections_max)
-    random.shuffle(room_names)
+
+def room_name_to_room_index(room_names, key):
+    i = 0
+    for name in room_names:
+        if key == name:
+            return i
+        else:
+            i += 1
+
+
+def build_room_connections(room_names, room_connections, rand_gen):
+    """
+    >>> build_room_connections([1,2,3], [[],[],[]], lambda: 2)
+    [[2, 3], [1, 3], [1, 2]]
+    """
+    rooms_left = room_names[:]
+    for this_room_index, this_room in enumerate(room_names):
+        rooms_left = rooms_left[1:]
+        number_of_connections = rand_gen()
+        for i, room in enumerate(rooms_left[:]):
+            if i >= number_of_connections:
+                break
+            if room not in room_connections[this_room_index]:
+                other_room_index = room_name_to_room_index(room_names, room)
+                room_connections[this_room_index].append(room)
+                if this_room not in room_connections[other_room_index]:
+                    room_connections[other_room_index].append(this_room)
+    return room_connections
+
+
+def build_a_room(f, the_dir_name, room_connections, room_name, start=False, end=False):
     room_type = room_types[0]
     if start:
         room_type = room_types[1]
@@ -54,7 +85,7 @@ def build_a_room(f, the_dir_name, room_name, start=False, end=False):
         room_type = room_types[2]
 
     f.write(room_file_format_header % room_name)
-    connections = room_names[:number_of_connections]
+    connections = room_connections[room_name_to_room_index(room_names, room_name)]
     for number, name in enumerate(connections):
         f.write(room_connection_format % (number, name))
     f.write(room_file_format_footer % room_type)
@@ -77,13 +108,13 @@ def build_a_room(f, the_dir_name, room_name, start=False, end=False):
     f2.write(prompt_footer)
 
 
-def build_rooms():
+def build_rooms(_room_connections):
     the_dir_name = dir_name % os.getpid()
     if not os.path.isdir(the_dir_name):
         os.mkdir(the_dir_name)
     for i in range(0, number_of_rooms):
         f = open(the_dir_name + "/room%d.adventure.txt" % (i + 1), 'w')
-        build_a_room(f, the_dir_name, room_names2[i], start=(i == 0), end=(i == 1))
+        build_a_room(f, the_dir_name, _room_connections, room_names[i], start=(i == 0), end=(i == 1))
         f.close()
 
 
@@ -104,7 +135,7 @@ def enter_room(room_name):
             print(error_msg)
             print(buf)
     steps += 1
-    if path == room_names2[1]:
+    if path == room_names[1]:
         return path
     else:
         return path + "\n" + enter_room(path)
@@ -112,11 +143,17 @@ def enter_room(room_name):
 
 def main():
     global steps
-    random.shuffle(room_names2)
-    build_rooms()
+    random.shuffle(room_names)
+    rand_gen = lambda: random.randint(connections_min, connections_max)
+    _room_connections = build_room_connections(room_names, room_connections, rand_gen)
+    build_rooms(_room_connections)
     buf = enter_room("START")
     print(winning_message % (steps, buf))
-    return 0
+    # #include <stdlib.h>
+    # "Cheating" a bit here since I don't think cleanup
+    # was a primary part of the assignment
+
+    # system("rm -fr %s" % the_dir_name)
 
 
 if __name__ == "__main__":
