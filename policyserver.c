76c91d13b47d63519f8d3be4ba390192e6b78321
Andy Chase <asperous2@gmail.com>
2015-07-24 16:15:19 -0700

    Add threaded chatserv implementation

--- policyserver.c
+++ policyserver.c
@@ -1,4 +1,4 @@
-/* Chasean / CS 372 / chatserv
+#include <sys/cdefs.h>/* Chasean / CS 372 / chatserv
  * References used:
  * - My Flash Policy Server: https://github.com/andychase/FlashPolicyServer
  * - Signal tutorial: http://www.yolinux.com/TUTORIALS/C++Signals.html
@@ -8,16 +8,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
-#define MAX_BUFFER_SIZE      1028
+#define BUFFER_SIZE      1028
+
+int portNumber = 8080;
+const char *quitMsg = "\\quit";
+
+const char *usage = "Usage: chanserv [port #]";
+const char *hostHandle = "SERV> ";
 
 int socketFd;
-char buffer[MAX_BUFFER_SIZE];
-const char *quitMsg = "\\quit";
-size_t quitMsgSize = 6; // String size + \0
-const char *usage = "Usage: chanserv [port #]";
-int portNumber = 8080;
-const char *hostHandle = "SERV> ";
+
+// Read/write pipe pair for communication with a socket
+int pipes[2][2];
+#define READ 0
+#define WRITE 1
+#define SERVER_SEND 0
+#define CLIENT_RECEIVE 1
 
 void signal_callback_handler(int signalNumber) {
     printf("Caught signal %d, cleaning up\n", signalNumber);
@@ -25,9 +33,67 @@ void signal_callback_handler(int signalNumber) {
     exit(0);
 }
 
+void *writeConsoleToSocket(void __unused *_) {
+    int clientFd;
+    ssize_t sendSuccess;
+    char *readFromConsoleSuccess;
+    char inputBuffer[BUFFER_SIZE];
+
+    while (1) {
+        /* --- Wait for, and recieve a client ---- */
+        read(pipes[SERVER_SEND][READ], &clientFd, sizeof(int));
+
+        sendSuccess = 1;
+        while (sendSuccess > 0 && !strstr(inputBuffer, quitMsg)) {
+            /* --- Print prompt --- */
+            printf("%s", hostHandle);
+            /* --- Read message from standard input --- */
+            readFromConsoleSuccess = fgets(inputBuffer, BUFFER_SIZE, stdin);
+            if (!readFromConsoleSuccess)
+                break;
+            /* --- Send --- */
+            sendSuccess = send(clientFd, inputBuffer, strlen(inputBuffer), 0);
+        }
+        // If user wrote the quit message, close socket
+        if (strstr(inputBuffer, quitMsg))
+            close(clientFd);
+        inputBuffer[0] = '\0';
+    }
+}
+
+void *readSocketToConsole(void __unused *_) {
+    int clientFd;
+    ssize_t receiveSuccess;
+    char inputBuffer[BUFFER_SIZE];
+
+    while (1) {
+        /* --- Wait for, and recieve a client ---- */
+        read(pipes[CLIENT_RECEIVE][READ], &clientFd, sizeof(int));
+
+        receiveSuccess = 1;
+        while (receiveSuccess != 0) {
+            /* --- Receive --- */
+            receiveSuccess = recv(clientFd, inputBuffer, BUFFER_SIZE, 0);
+            /* --- Print --- */
+            printf("\n%s%s", inputBuffer, hostHandle);
+            fflush(stdout);
+        }
+        inputBuffer[0] = '\0';
+        printf("\n%s\n", "Client disconnected");
+    }
+
+}
 
 int main(int argc, char *argv[]) {
     signal(SIGINT, signal_callback_handler);
+
+    /* --- Set up client handler threads ---*/
+    pthread_t threads[2];
+    // Create pipes for sending clients
+    pipe(pipes[CLIENT_RECEIVE]);
+    pipe(pipes[SERVER_SEND]);
+    pthread_create(&threads[0], NULL, writeConsoleToSocket, NULL);
+    pthread_create(&threads[0], NULL, readSocketToConsole, NULL);
 
     /* --- Read port from arguments or print usage -- */
     if (argc < 2 || atoi(argv[1]) == 0) {
@@ -67,28 +133,12 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in client_address;
     socklen_t addressLength = sizeof(client_address);
     int clientFd;
-    ssize_t receiveSuccess;
     while (1) {
         /* --- Accept a client --- */
         clientFd = accept(socketFd, (struct sockaddr *) &client_address, &addressLength);
-        receiveSuccess = 1;
-
-        while (receiveSuccess != 0 && !strstr(buffer,quitMsg)) {
-            /* --- Receive --- */
-            receiveSuccess = recv(clientFd, buffer, MAX_BUFFER_SIZE, 0);
-
-            /* --- Msg --- */
-            printf("%s", hostHandle);
-            fflush(stdout);
-
-            fgets(buffer, MAX_BUFFER_SIZE, stdin);
-            /* --- Send --- */
-            send(clientFd, quitMsg, quitMsgSize, 0);
-        }
-        buffer[0] = '\0';
-
-        /* --- Close ---*/
-        close(clientFd);
+        printf("%s\n", "Client connected");
+        /* --- Send client to handler threads --- */
+        write(pipes[CLIENT_RECEIVE][WRITE], &clientFd, sizeof(int));
+        write(pipes[SERVER_SEND][WRITE], &clientFd, sizeof(int));
     }
 }
-
