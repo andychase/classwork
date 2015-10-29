594e842c17acd28884c2c3c60b2c50ca71d0e8de
Andy Chase <asperous2@gmail.com>
2015-10-28 20:09:53 -0700

    reformat rockpaperscissors script

--- assignment3/script.js
+++ assignment3/script.js
@@ -3,45 +3,48 @@
 // Using Math.round() will give you a non-uniform distribution!
 // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// Calculate results
+// true if user won, false if not, undefined if tie
+function didUserWin(userChoice, computerChoice) {
+    if (userChoice == "rock" && computerChoice == "paper") {
+        return false;
+    } else if (userChoice == "rock" && computerChoice == "scissors") {
+        return true;
+    } else if (userChoice == "paper" && computerChoice == "rock") {
+        return true;
+    } else if (userChoice == "paper" && computerChoice == "scissors") {
+        return false;
+    } else if (userChoice == "scissors" && computerChoice == "rock") {
+        return false;
+    } else if (userChoice == "scissors" && computerChoice == "paper") {
+        return true;
+    }
+}
 
 // Update Score
 function updateScoreboard(userWon) {
-	var score;
-	if (userWon) {
-		s = document.getElementById("user-score-number");
-	} else {
-		s = document.getElementById("computer-score-number");
-	}
-	s.innerHtml = s.innerText++;
+    var score;
+    if (userWon) {
+        score = document.getElementById("user-score-number");
+    } else {
+        score = document.getElementById("computer-score-number");
+    }
+    score.innerHtml = score.innerText++;
 }
 
 // Show results
 function getComputerResult() {
-	switch(getRandomInt(0, 3)) {
-		case 0: return "rock";
-		case 1: return "paper";
-		case 2: return "scissors";
-	};
-}
-
-// Finish results
-function didUserWin(userChoice, computerChoice) {
-	if (userChoice == "rock" && computerChoice == "paper") {
-		return false;
-	} else if (userChoice == "rock" && computerChoice == "scissors") {
-		return true;
-	} else if (userChoice == "paper" && computerChoice == "rock") {
-		return true;
-	} else if (userChoice == "paper" && computerChoice == "scissors") {
-		return false;
-	} else if (userChoice == "scissors" && computerChoice == "rock") {
-		return false;
-	} else if (userChoice == "scissors" && computerChoice == "paper") {
-		return true;
-	}
+    switch (getRandomInt(0, 3)) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
 }
 
 // Show results
@@ -49,45 +52,51 @@ var showTimerUsr;
 var showTimerCmp;
 var resultsTime = 2000;
 function showResult(userChoice, computerChoice, userWon) {
-	document.getElementById("c-usr").className =
-		"choice fa show fa-hand-" + userChoice + "-o " + userWon;
-	if (typeof showTimerUsr != "undefined") 
-		window.clearTimeout(showTimerUsr);
-	showTimerUsr = window.setTimeout(function () {
-		document.getElementById("c-usr").className =
-			"choice fa fa-hand-" + userChoice + "-o";
-	}, resultsTime);
+    // Update the user result
+    document.getElementById("c-usr").className =
+        "choice fa show fa-hand-" + userChoice + "-o " + userWon;
 
-	document.getElementById("c-cmp").className =
-		"choice fa show fa-hand-" + computerChoice + "-o " + userWon;
-	if (typeof showTimerCmp != "undefined") 
-		window.clearTimeout(showTimerCmp);
-	showTimerCmp = window.setTimeout(function () {
-		document.getElementById("c-cmp").className =
-			"choice fa fa-hand-" + computerChoice + "-o";
-	}, resultsTime);
+    // Fade out user result
+    if (typeof showTimerUsr != "undefined")
+        window.clearTimeout(showTimerUsr);
+    showTimerUsr = window.setTimeout(function () {
+        document.getElementById("c-usr").className =
+            "choice fa fa-hand-" + userChoice + "-o";
+    }, resultsTime);
+
+    // Update the computer result
+    document.getElementById("c-cmp").className =
+        "choice fa show fa-hand-" + computerChoice + "-o " + userWon;
+    // Fade out computer result
+    if (typeof showTimerCmp != "undefined")
+        window.clearTimeout(showTimerCmp);
+    showTimerCmp = window.setTimeout(function () {
+        document.getElementById("c-cmp").className =
+            "choice fa fa-hand-" + computerChoice + "-o";
+    }, resultsTime);
 }
 
-// Wait until the DOM is ready
-// This does not work in IE8
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var select = document.getElementsByClassName('select');
-  for (s in select) if (select.hasOwnProperty(s)) {
-  	select[s].addEventListener("click", function (event) {
-  		var computerChoice = getComputerResult();
-  		var userWon;
-  		if (event.target.classList.contains("rock")) {
-  			userWon = didUserWin("rock", computerChoice);
-  			showResult("rock", computerChoice, userWon);
-  		} else if (event.target.classList.contains("paper")) {
-  			userWon = didUserWin("paper", computerChoice);
-			showResult("paper", computerChoice, userWon);
-  		} else {
-			userWon = didUserWin("scissors", computerChoice);
-			showResult("scissors", computerChoice, userWon);
-  		}
-  		if (userWon != undefined)
-  			updateScoreboard(userWon);
-  	});
-  }
+//
+// Main game code
+document.addEventListener("DOMContentLoaded", function (event) {
+    var select = document.getElementsByClassName('select');
+    var s;
+    for (s in select) if (select.hasOwnProperty(s)) {
+        select[s].addEventListener("click", function (event) {
+            var computerChoice = getComputerResult();
+            var userWon;
+            if (event.target.classList.contains("rock")) {
+                userWon = didUserWin("rock", computerChoice);
+                showResult("rock", computerChoice, userWon);
+            } else if (event.target.classList.contains("paper")) {
+                userWon = didUserWin("paper", computerChoice);
+                showResult("paper", computerChoice, userWon);
+            } else {
+                userWon = didUserWin("scissors", computerChoice);
+                showResult("scissors", computerChoice, userWon);
+            }
+            if (userWon != undefined)
+                updateScoreboard(userWon);
+        });
+    }
 });
