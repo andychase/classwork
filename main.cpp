f7f52eba6396cba43f7c1e9a65c7d65c0b162815
Andy Chase <asperous2@gmail.com>
2015-11-09 17:21:20 -0800

    Brute force key

--- main.cpp
+++ main.cpp
@@ -1,60 +1,58 @@
 #include <stdio.h>
 #include <openssl/evp.h>
-#include <string.h>
-#include <unordered_set>
-#include <string>
 
+unsigned char key[1024], outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
+
+// Usage: cat words.txt | /Users/a5c/Library/Caches/CLion12/cmake/generated/6aca852e/6aca852e/Debug/problem1 | grep 8d20e5
+
+// Adopted from: https://www.openssl.org/docs/manmaster/crypto/EVP_EncryptInit.html
+int do_crypt(int do_encrypt) {
+    /* Allow enough space in output buffer for additional block */
+    int inlen = 0;
+    int outlen;
+    EVP_CIPHER_CTX ctx;
+    /* Bogus key and IV: we'd normally set these from
+     * another source.
+     */
+    unsigned char msg[] = "This is a top secret.";
+    unsigned char iv[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    /* Don't set key or IV right away; we want to check lengths */
+    EVP_CIPHER_CTX_init(&ctx);
+    EVP_CipherInit_ex(&ctx, EVP_aes_128_cbc(), NULL, NULL, NULL, do_encrypt);
+    OPENSSL_assert(EVP_CIPHER_CTX_key_length(&ctx) == 16);
+    OPENSSL_assert(EVP_CIPHER_CTX_iv_length(&ctx) == 16);
+
+    /* Now we can set key and IV */
+    EVP_CipherInit_ex(&ctx, NULL, NULL, key, iv, do_encrypt);
+
+    if (!EVP_CipherUpdate(&ctx, outbuf, &outlen, msg, 21)) {
+        /* Error */
+        EVP_CIPHER_CTX_cleanup(&ctx);
+        return 0;
+    }
+    for (int i = 0; i < outlen; i++)
+        printf("%02x", outbuf[i]);
+    if (!EVP_CipherFinal_ex(&ctx, outbuf, &outlen)) {
+        /* Error */
+        EVP_CIPHER_CTX_cleanup(&ctx);
+        return 0;
+    }
+    for (int i = 0; i < outlen; i++)
+        printf("%02x", outbuf[i]);
+    printf("\n");
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    return 1;
+}
 
 int main(int argc, char *argv[]) {
-    std::unordered_set<std::string> myset;
-    std::string my_value_copy;
-    EVP_MD_CTX *mdctx;
-    const EVP_MD *md;
-    char mess[32];
-    unsigned char md_value[EVP_MAX_MD_SIZE];
-    int md_len;
-    srand((unsigned int) time(NULL));
-
-
-    OpenSSL_add_all_digests();
-
-    md = EVP_get_digestbyname("sha256");
-
-    if (!md) {
-        printf("Unknown message digest %s\n", argv[1]);
-        exit(1);
+    // Adopted from https://stackoverflow.com/questions/12252103/read-line-from-stdin-blocking
+    char *line = NULL;
+    size_t size;
+    while (getline(&line, &size, stdin) != -1) {
+        snprintf((char *) key, 17, "%s", line);
+        printf("%s: ", key);
+        do_crypt(1);
     }
-
-    printf("Rounds to find a 3 byte hash collision \n");
-    for (int t = 0; t < 1000; t++) {
-        unsigned int rounds = 0;
-        long nonce = rand() + (rand() * 20720703);
-        myset.clear();
-        do {
-
-            snprintf(mess, 32, "%lu", nonce);
-            mdctx = EVP_MD_CTX_create();
-            EVP_DigestInit_ex(mdctx, md, NULL);
-            EVP_DigestUpdate(mdctx, mess, strlen(mess));
-            EVP_DigestFinal_ex(mdctx, md_value, (unsigned int *) &md_len);
-            EVP_MD_CTX_destroy(mdctx);
-            rounds++;
-            nonce++;
-            my_value_copy.clear();
-            my_value_copy += md_value[0];
-            my_value_copy += md_value[1];
-            my_value_copy += md_value[2];
-
-            if (myset.count(my_value_copy) == 1)
-                break;
-            myset.insert(my_value_copy);
-
-        } while (true);
-
-        printf("Rounds: %d\n", rounds);
-    }
-
-    /* Call this once before exit. */
-    EVP_cleanup();
-    exit(0);
+    return 0;
 }
