541a1b119eb40d3ed17560fa686518156cd7d3b3
Andy Chase <asperous2@gmail.com>
2015-12-05 18:12:16 -0800

    Add notification label

--- finalProject/SignInViewController.swift
+++ finalProject/SignInViewController.swift
@@ -13,19 +13,23 @@ class SignInViewController: UIViewController {
     
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var notificationLabel: UILabel!
     
     @IBAction func signInButtonPress(sender: UIButton) {
-        let parameters = ["username": usernameField.text!, "password": passwordField.text!]
-        Alamofire.request(.POST, "https://requestbin.herokuapp.com/1atd5h51", parameters: parameters)
-            .responseJSON { response in
-                print(response.request)  // original URL request
-                print(response.response) // URL response
-                print(response.data)     // server data
-                print(response.result)   // result of response serialization
-                
-                if let JSON = response.result.value {
-                    print("JSON: \(JSON)")
-                }
-        }
+//        let parameters = ["username": usernameField.text!, "password": passwordField.text!]
+//        Alamofire.request(.POST, "https://requestbin.herokuapp.com/1atd5h51", parameters: parameters)
+//            .responseJSON { response in
+//                print(response.request)  // original URL request
+//                print(response.response) // URL response
+//                print(response.data)     // server data
+//                print(response.result)   // result of response serialization
+//                
+//                if let JSON = response.result.value {
+//                    print("JSON: \(JSON)")
+//                }
+//        }
+        notificationLabel.hidden = false;
+        notificationLabel.text = "Sign in Success!"
+        notificationLabel.textColor = UIColor.greenColor()
     }
 }
