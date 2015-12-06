0eaf30600d0992b6d5e58e174311697471c7cf43
Andy Chase <asperous2@gmail.com>
2015-12-05 16:56:36 -0800

    Connect up buttons

---
+++ finalProject/SignInViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.swift
+//  finalProject
+//
+//  Created by Andy Chase on 12/5/15.
+//  Copyright © 2015 Andy Chase. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func signInButtonPress(sender: AnyObject) {
+        
+    }
+}
+
