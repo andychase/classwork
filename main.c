348dc2c65d1b132291abe9aa67ece8fc2f11afae
Andy Chase <asperous2@gmail.com>
2016-04-07 19:40:32 -0700

    Stub consumers/producers functions and add pipe.

--- main.c
+++ main.c
@@ -1,37 +1,70 @@
-/******************************************************************************
-* FILE: main.c
-* DESCRIPTION:
-*   A "hello world" Pthreads program.  Demonstrates thread creation and
-*   termination.
-* AUTHOR: Blaise Barney
-* LAST REVISED: 08/09/11
-******************************************************************************/
+/*
+
+
+REFERENCES:
+* https://computing.llnl.gov/tutorials/pthreads/samples/hello.c
+* http://mij.oltrelinux.com/devel/unixprg/
+* https://stackoverflow.com/questions/1620918/cmake-and-libpthread
+* http://www.tutorialspoint.com/cprogramming/c_structures.htm
+
+*/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define NUM_THREADS    2
 
-void *PrintHello(void *threadid) {
+struct WorkUnit {
+    unsigned int unitNumber;
+    unsigned int workTime;
+};
+
+int workPipe[2];
+
+#define readPipe workPipe[0]
+#define writePipe workPipe[1]
+
+void *consumer(void *threadId) {
+    struct WorkUnit work_message;
     long tid;
-    tid = (long) threadid;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
+    ssize_t ret;
+    tid = (long) threadId;
+
+    for (; ;) {
+        ret = read(readPipe, &work_message, sizeof(struct WorkUnit));
+        if (ret == 0)
+            break;
+        sleep(work_message.workTime);
+        printf("Thread #%ld consumed %d. Took %d seconds.\n", tid, work_message.unitNumber, work_message.workTime);
+
+    };
     pthread_exit(NULL);
+}
+
+void producer() {
+    struct WorkUnit producer_message = {.unitNumber = 1, .workTime = 1};
+    write(writePipe, &producer_message, sizeof(struct WorkUnit));
+    producer_message.unitNumber++;
+    close(writePipe);
 }
 
 int main(int argc, char *argv[]) {
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
+
+    pipe(workPipe);
+
     for (t = 0; t < NUM_THREADS; t++) {
         printf("In main: creating thread %ld\n", t);
-        rc = pthread_create(&threads[t], NULL, PrintHello, (void *) t);
+        rc = pthread_create(&threads[t], NULL, consumer, (void *) t);
         if (rc) {
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
     }
 
-    /* Last thing that main() should do */
+    producer();
     pthread_exit(NULL);
 }
