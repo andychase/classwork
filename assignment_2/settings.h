51a5711cf522d5d0fed042222798524934009c74
Andy Chase <asperous2@gmail.com>
2016-04-18 12:08:44 -0700

    init

---
+++ assignment_2/settings.h
@@ -0,0 +1,14 @@
+//
+// Created by Andy Chase on 4/18/16.
+//
+
+#ifndef ASSIGNMENT_2_SETTINGS_H
+#define ASSIGNMENT_2_SETTINGS_H
+
+#define NUMT            4
+#define NUMS            4
+#define ARRAYSIZE       1000
+#define NUMTRIES        20
+
+
+#endif //ASSIGNMENT_2_SETTINGS_H
