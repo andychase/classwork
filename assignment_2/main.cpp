a925816507eaa017ca989e77297fc4ab528af054
Andy Chase <asperous2@gmail.com>
2016-04-18 21:13:01 -0700

    Project #2 longer chart

--- assignment_2/main.cpp
+++ assignment_2/main.cpp
@@ -8,33 +8,43 @@ float Height(int, int);
 
 
 int main(int argc, char *argv[]) {
-    printf("NUMT, NUMS, sum, MegaHeightsPerSec\n");
+    printf("thread,");
+    for (NUMS = 2; NUMS <= 32768; NUMS = NUMS * 2) {
+        printf("%i,", NUMS);
+    }
     for (NUMT = 1; NUMT <= 8; NUMT++) {
-        for (NUMS = 2; NUMS <= 16384; NUMS = NUMS * 2) {
+        printf("\n%i,", NUMT);
+        for (NUMS = 2; NUMS <= 32768; NUMS = NUMS * 2) {
+            double time_total = 0;
+            for (int tries = 0; tries < 4; tries++) {
+                double start_time = omp_get_wtime();
+                omp_set_num_threads(NUMT);
 
-            const float fullTileArea = (((XMAX - XMIN) / (float) (NUMS - 1)) * ((YMAX - YMIN) / (float) (NUMS - 1)));
-            float weight = 1;
-            double sum = 0;
 
-            omp_set_num_threads(NUMT);
+                const float fullTileArea = (((XMAX - XMIN) / (float) (NUMS - 1)) *
+                                            ((YMAX - YMIN) / (float) (NUMS - 1)));
 
-            double start_time = omp_get_wtime();
+                float weight = 1;
+                double sum = 0;
 
 #pragma omp parallel for collapse(2) private(weight) reduction(+:sum)
-            for (int iv = 0; iv < NUMS; iv++) {
-                for (int iu = 0; iu < NUMS; iu++) {
-                    if ((iv == 0 || iv == NUMS - 1) && (iu == 0 || iu == NUMS - 1))
-                        weight = .25;
-                    else if ((iv == 0 || iv == NUMS - 1) || (iu == 0 || iu == NUMS - 1))
-                        weight = .5;
-                    else
-                        weight = 1;
-                    sum += fullTileArea * weight * Height(iv, iu);
+                for (int iv = 0; iv < NUMS; iv++) {
+                    for (int iu = 0; iu < NUMS; iu++) {
+                        if ((iv == 0 || iv == NUMS - 1) && (iu == 0 || iu == NUMS - 1))
+                            weight = .25;
+                        else if ((iv == 0 || iv == NUMS - 1) || (iu == 0 || iu == NUMS - 1))
+                            weight = .5;
+                        else
+                            weight = 1;
+                        sum += fullTileArea * weight * Height(iv, iu);
+                    }
                 }
+                double end_time = omp_get_wtime();
+                time_total += end_time - start_time;
             }
-            double end_time = omp_get_wtime();
-            double MegaHeightsPerSec = NUMS * NUMS / (end_time - start_time) / 1000000;
-            printf("%i, %i, %f, %8.2lf\n", NUMT, NUMS, sum, MegaHeightsPerSec);
+            double MegaHeightsPerSec = NUMS * NUMS / (time_total) / 4 / 1000000;
+            printf("%.2lf,", MegaHeightsPerSec);
+            fflush(stdout);
         }
     }
 }
