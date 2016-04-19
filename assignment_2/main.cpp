068a782f77617fe089e2617c44d969c2b1fa416a
Andy Chase <asperous2@gmail.com>
2016-04-18 21:11:57 -0700

    # This is a combination of 2 commits. # The first commit's message is:
    Project #2 implementation

--- assignment_2/main.cpp
+++ assignment_2/main.cpp
@@ -8,24 +8,24 @@ float Height(int, int);
 
 
 int main(int argc, char *argv[]) {
-    printf("NUMT, NUMS, sum, MegaHeightsPerSec");
+    printf("NUMT, NUMS, sum, MegaHeightsPerSec\n");
     for (NUMT = 1; NUMT <= 8; NUMT++) {
-        for (NUMS = 2; NUMS <= 16384; NUMS = NUMS*2) {
-            double start_time = omp_get_wtime();
+        for (NUMS = 2; NUMS <= 16384; NUMS = NUMS * 2) {
+
+            const float fullTileArea = (((XMAX - XMIN) / (float) (NUMS - 1)) * ((YMAX - YMIN) / (float) (NUMS - 1)));
+            float weight = 1;
+            double sum = 0;
+
             omp_set_num_threads(NUMT);
 
-
-            const double fullTileArea = (((XMAX - XMIN) / (float) (NUMS - 1)) * ((YMAX - YMIN) / (float) (NUMS - 1)));
-
-            float weight = 1;
-            float sum = 0;
+            double start_time = omp_get_wtime();
 
 #pragma omp parallel for collapse(2) private(weight) reduction(+:sum)
             for (int iv = 0; iv < NUMS; iv++) {
                 for (int iu = 0; iu < NUMS; iu++) {
-                    if ((iv == 0 || iv == NUMS) && (iu == 0 || iu == NUMS))
+                    if ((iv == 0 || iv == NUMS - 1) && (iu == 0 || iu == NUMS - 1))
                         weight = .25;
-                    else if ((iv == 0 || iv == NUMS) || (iu == 0 || iu == NUMS))
+                    else if ((iv == 0 || iv == NUMS - 1) || (iu == 0 || iu == NUMS - 1))
                         weight = .5;
                     else
                         weight = 1;
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
                 }
             }
             double end_time = omp_get_wtime();
-            double MegaHeightsPerSec = NUMS*NUMS / (end_time - start_time) / 1000000;
+            double MegaHeightsPerSec = NUMS * NUMS / (end_time - start_time) / 1000000;
             printf("%i, %i, %f, %8.2lf\n", NUMT, NUMS, sum, MegaHeightsPerSec);
         }
     }
