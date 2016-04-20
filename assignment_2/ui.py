23c417f9b1e8076c5e80176582a095d39f447c5d
Andy Chase <asperous2@gmail.com>
2016-04-19 20:23:16 -0700

    Improve ui

--- assignment_2/ui.py
+++ assignment_2/ui.py
@@ -1,24 +1,32 @@
-face = "【 ツ 】"
-thinking = "【 ? 】"
-eating = "【:()】"
-fork = "Y"
+use_ascii = True
+
+if use_ascii:
+    face = " =]"
+    thinking = "THK"
+    eating = "EAT"
+    fork_icon = "Y"
+else:
+    face = "😀"
+    thinking = "🤔"
+    eating = "😖"
+    fork_icon = "🍴"
 
 philosophers = [
     "nothing",
-    "eating",
-    "thinking",
     "nothing",
-    "eating"
+    "nothing",
+    "nothing",
+    "nothing"
 ]
 
-ui_mapping = {
+philosopher_mapping = {
     "nothing": face,
     "eating": eating,
     "thinking": thinking
 }
 
 fork_mapping = {
-    True: fork,
+    True: fork_icon,
     False: ""
 }
 
@@ -33,9 +41,9 @@ forks = [
 
 def get_table():
     fork_display = [fork_mapping[fork] for fork in forks]
-    phi_mapping = [ui_mapping[i] for i in philosophers]
-    last_fork = "({})".format(fork_mapping[forks[-1]])
-    return "".join("".join(_) for _ in zip(fork_display, phi_mapping)) + last_fork
+    phi_mapping = [philosopher_mapping[i] for i in philosophers]
+    last_fork = "  {}".format(fork_mapping[forks[-1]])
+    return " ".join(" ".join(_) for _ in zip(fork_display, phi_mapping)) + last_fork
 
 
 def ui():
