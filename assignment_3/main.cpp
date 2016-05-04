21401428df57b7794e3a0b356eb7a19022bdca33
Andy Chase <asperous2@gmail.com>
2016-05-03 23:22:44 -0700

    fix #2

--- assignment_3/main.cpp
+++ assignment_3/main.cpp
@@ -1,33 +1,33 @@
 /* Copied from: http://web.engr.oregonstate.edu/~mjb/cs575/Projects/proj03.html */
 #include <omp.h>
 #include <stdio.h>
-#include "settings.h"
+
+#define NUMT    4
+
+struct {
+    float value;
+    int pad[17];
+} Array[NUMT];
 
 
 int main(int argc, char *argv[]) {
     omp_set_num_threads(NUMT);
 
-    unsigned int numberOfComputations = 10E7;    // if > 4B, use "long unsigned int"
+    long unsigned int numberOfComputations = 10E8;    // if > 4B, use "long unsigned int"
 
-
-    for (int padding = 0; padding < 20; padding++) {
-        struct s {
-            float value;
-            int pad[padding];
-        } Array[NUMT];
-        double start_time = omp_get_wtime();
-        for (int s = 0; s < REPEAT_TIMES; s++) {
+    double start_time = omp_get_wtime();
 #pragma omp parallel for
-            for (int i = 0; i < 4; i++) {
-                for (unsigned int j = 0; j < numberOfComputations; j++) {
-                    Array[i].value = Array[i].value + 2;
-                }
-            }
+        for (int i = 0; i < NUMT; i++) {
+            float tmp = Array[i].value;
+            for (unsigned int j = 0; j < numberOfComputations; j++) {
+                tmp += 2;
         }
-        double end_time = omp_get_wtime();
-        double time_total = end_time - start_time;
-
-        double MegaCompuationsPerSec = (numberOfComputations * REPEAT_TIMES) / (time_total) / 4 / 1000000;
-        printf("%i, %.2lf\n", padding, MegaCompuationsPerSec);
     }
+
+    double end_time = omp_get_wtime();
+    double time_total = end_time - start_time;
+
+    double MegaCompuationsPerSec = (numberOfComputations) / (time_total) / 4 / 1000000;
+    printf("%.2lf\n", MegaCompuationsPerSec);
+
 }
